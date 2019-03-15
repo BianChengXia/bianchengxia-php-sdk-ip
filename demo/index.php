2735c97391dc2df67b68bb56fcd5aa9670210f51
编程侠 <root@sysapi.com>
2019-03-15 10:23:18 +0800

    清理city_id的输出，直接显示返回数组

--- demo/index.php
+++ demo/index.php
@@ -1,9 +1,12 @@
 <?php
-//require_once '../src/Ip2Region.class.php';//使用composer无需手动引入
+require_once '../src/Ip2Region.class.php';//使用composer无需手动引入
 //$ip2region = new \Ip2Region();//适用于命名空间
 $ip2region = new Ip2Region();//适用于没有使用命名空间
 $ip = '113.205.63.37';
 $info = $ip2region->btreeSearch($ip);
+if (!empty($info)) {
+    $info = explode('|', $info);
+}
 //测试输出
 header('Content-Type: text/html; charset=utf8');
 print_r($info);
