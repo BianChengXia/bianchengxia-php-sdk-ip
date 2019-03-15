3faaa034e8190561a22f268720ed58174cef92bc
编程侠 <root@sysapi.com>
2019-03-15 08:18:52 +0800

    更新Demo适用于命名空间

--- demo/index.php
+++ demo/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../src/Ip2Region.class.php';
+//$ip2region = new \Ip2Region();//适用于命名空间
 $ip2region = new Ip2Region();
 $ip = '113.205.63.37';
 $info = $ip2region->btreeSearch($ip);
